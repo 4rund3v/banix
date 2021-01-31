5aba5648c7b22b207571db54eeda0b374f1703ed
arun dev <4rund3v@gmail.com>
2021-01-31 22:47:42 +0530

    header nav bar added

--- banix_ui/src/components/misc/Header.jsx
+++ banix_ui/src/components/misc/Header.jsx
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import {
   Navbar,
   Nav,
+  NavDropdown,
   Container,
   Image,
   Button,
   Form,
   FormControl,
+  Row,
+  Col,
+  InputGroup,
 } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,103 +19,35 @@ import { GUEST_USER_IMAGE } from "../../config";
 
 const Header = () => {
   return (
-    <Navbar variant="dark" bg="primary">
-      <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>
-            <BanixBanner />
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-bav">
-          <Nav className="align-items-center justify-content-center">
-            <Nav.Item>
-              <Form inline>
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                />
-                <Button variant="outline-primary">Search</Button>
-              </Form>
-            </Nav.Item>
-          </Nav>
-          <Nav className="ml-auto">
-            <LinkContainer to="/cart">
-              <Nav.Link>
-                <h4>
-                  <FontAwesomeIcon icon="shopping-cart" />
-                  Cart
-                </h4>
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>
-                <h4>
-                  <Image
-                    src={`${GUEST_USER_IMAGE}`}
-                    alt={"userimage"}
-                    height={20}
-                    width={20}
-                    fluid
-                  ></Image>
-                  Sign in
-                </h4>
-              </Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
+    <Container>
+      <Row>
+        <Col>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">banix</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <InputGroup>
+                  <FormControl
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="basic-addon1"
+                  />
+                  <InputGroup.Append>
+                    <div class="input-group-text bg-transparent">
+                      <FontAwesomeIcon icon="search" />
+                    </div>
+                  </InputGroup.Append>
+                </InputGroup>
 
-        {/* <Collapse  navbar> isOpen={this.state.isOpen}
-                      
-            <Nav className="align-items-center justify-content-center">
-              <NavItem>
-                <div id="searchBox">{ <SearchForm /> }</div>
-              </NavItem>
-            </Nav>
-            <Nav className="ml-auto align-items-center justify-content-center">
-              <NavItem>
-                <div id="cartCount">
-                  {<Link to="/cart" className="text-light">
-                    <FontAwesomeIcon icon="shopping-cart" />
-                    <span className="font-weight-bold"> Cart</span> (
-                    {this.cartItemCount()})
-                  </Link> }
-                </div>
-              </NavItem>
-            </Nav>
-            <Nav className="align-items-center justify-content-center">
-              <NavItem>
-                {this.props.isAuthenticated ? (
-                  <ButtonGroup id="authBtnGroup">
-                    {<Link to="/profile" className="btn btn-sm btn-success">
-                      {decoded_token.username !== "" // if user has username...
-                        ? decoded_token.username // display it or use email
-                        : decoded_token.email}{" "}
-                      <FontAwesomeIcon icon="user" />
-                    </Link> }
-                    <Button
-                      className="btn btn-sm btn-warning"
-                      onClick={this.props.authLogout}
-                    >
-                      Logout <FontAwesomeIcon icon="sign-out-alt" />
-                    </Button>
-                  </ButtonGroup>
-                ) : (
-                  <ButtonGroup>
-                    <Link to="/login" className="btn btn-sm btn-warning">
-                      Login <FontAwesomeIcon icon="sign-in-alt" />
-                    </Link>
-                    <Link to="/register" className="btn btn-sm btn-info">
-                      Register <FontAwesomeIcon icon="user-plus" />
-                    </Link>
-                  </ButtonGroup>
-                )}
-              </NavItem>
-            </Nav>
-          </Collapse>  */}
-      </Container>
-    </Navbar>
+                <Nav.Link href="#home">Cart</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- banix_ui/src/config.js
+++ banix_ui/src/config.js
@@ -1,7 +1,7 @@
-export const IMAGE_URL = `http://potatocodes.in:8086`;
+export const IMAGE_URL = process.env.REACT_APP_IMAGE_SERVER_URL;
 export const HEADER_IMAGE = `${IMAGE_URL}/assets/header__logo.png`;
 export const GUEST_USER_IMAGE = `${IMAGE_URL}/assets/guest_user.svg`;
 
-export const BASE_API_URL = `http://localhost:3300`;
+export const BASE_API_URL = process.env.REACT_APP_API_SERVER_URL;
 export const PRODUCT_LIST_URL = `/api/products`;
 export const PRODUCT_SPECIFIC_URL = `/api/products`;
