14be86a1c89d41db019d30abcd0ae634d5cb4a17
arun dev <4rund3v@gmail.com>
2021-02-14 23:07:48 +0530

    price component updated in products ui.

--- banix_ui/src/components/store/ProductPrice.jsx
+++ banix_ui/src/components/store/ProductPrice.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
 const ProductPrice = ({ product }) => {
+  const discountPercentage = Number(
+    (Number(product.productCostPrice) - Number(product.productSellingPrice)) /
+      100
+  );
   return (
     <>
       {product.productSellingPrice < product.productCostPrice ? (
         <span>
           <span className="text-secondary">Price:</span>{" "}
-          <span className="text-danger">
+          <span className="product_price_main">
             &#8377;
             {product.productSellingPrice}
           </span>{" "}
-          <del className="text-muted">
+          <del className="product_price_muted">
             &#8377;
             {product.productCostPrice}
           </del>
+          <span className="product_price_discount">
+            &#x00025;{discountPercentage} {"off"}
+          </span>{" "}
         </span>
       ) : (
-        <span>
+        <span className="product_price_main_default">
           &#8377;
           {product.productCostPrice}
         </span>
