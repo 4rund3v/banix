81779293474a2b13c02684f16f62049833a858e8
arun dev <4rund3v@gmail.com>
2021-02-19 20:13:01 +0530

    [UI] removed the url prefix and the config urls.

--- banix_ui/src/config.js
+++ banix_ui/src/config.js
@@ -1,18 +1,13 @@
-export const IMAGE_URL = process.env.REACT_APP_IMAGE_SERVER_URL;
-export const HEADER_IMAGE = `${IMAGE_URL}/assets/header__logo.png`;
-export const GUEST_USER_IMAGE = `${IMAGE_URL}/assets/guest_user.svg`;
-
 // Base URL info
-export const BASE_API_URL = process.env.REACT_APP_SERVER_URL;
-export const PRODUCT_LIST_URL = `${BASE_API_URL}/api/products`;
-export const PRODUCT_SPECIFIC_URL = `${BASE_API_URL}/api/products`;
-export const SERVICEABILITY_URL = `${BASE_API_URL}/api/serviceability`;
+export const PRODUCT_LIST_URL = `/api/products`;
+export const PRODUCT_SPECIFIC_URL = `/api/products`;
+export const SERVICEABILITY_URL = `/api/serviceability`;
 
-export const CUSTOMER_LOGIN_URL = `${BASE_API_URL}/api/customers/login`;
-export const CUSTOMER_REGISTER_URL = `${BASE_API_URL}/api/customers/register`;
-export const CUSTOMER_PROFILE_URL = `${BASE_API_URL}/api/customers/profile`;
+export const CUSTOMER_LOGIN_URL = `/api/customers/login`;
+export const CUSTOMER_REGISTER_URL = `/api/customers/register`;
+export const CUSTOMER_PROFILE_URL = `/api/customers/profile`;
 
-export const CREATE_ORDER_URL = `${BASE_API_URL}/api/orders`;
+export const CREATE_ORDER_URL = `/api/orders`;
 
 // Social MediaLinks
 export const BANIX_YOUTUBE_URL =
